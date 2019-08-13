0e6c587807fbcdc77eb9d67078748d71d6c711f5
RochanaRamanathan <54059882+RochanaRamanathan@users.noreply.github.com>
2019-08-13 14:44:30 +0530

    Update and rename prog2.py to prog3.py

--- prog2.py
+++
@@ -1,5 +0,0 @@
-a=eval (input())
-if (a%2==0):
-  print("Even")
-else:
-  print("Odd")
---
+++ prog3.py
@@ -0,0 +1,8 @@
+a=eval (input())
+if (a<0):
+  print("Invalid")
+elif (a%2==0):
+  print("Even")
+elif (a%2==1):
+  print ("Odd")   
+  
